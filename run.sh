5c575607285ce6d095751ce512f377032b6c0222
YOUlfey <gajdar97artem@gmail.com>
2019-03-30 00:56:26 +0400

    refactoring

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 mkdir -p res/data/archives
 mkdir -p res/data/extract
-wget http://datasets.d2.mpi-inf.mpg.de/MPIIgaze/MPIIGaze.tar.gz -P res/data/archives
+wget http://datasets.d2.mpi-inf.mpg.de/MPIIGaze/MPIIGaze.tar.gz -P res/data/archives
 tar xzvf res/data/archives/MPIIGaze.tar.gz -C res/data/extract
 rm -r res/data/archives
 python3 gaze-preprocess.py
